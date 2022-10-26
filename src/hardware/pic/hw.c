52bd04e7a734ed29ebd3dee68da825393165468f
Luciano Martorella <mad_lux_it@yahoo.it>
2022-10-26 22:14:51 +0200

    - Renamed inc to c

--- src/hardware/pic/hw.c
+++ src/hardware/pic/hw.c
@@ -5,13 +5,13 @@
  */
 
 #ifdef _IS_ETH_CARD
-#include "fuses_eth_card.inc"
+#include "fuses_eth_card.c"
 #elif defined(_IS_PIC16F628_CARD)
-#include "fuses_pic16f628.inc"
+#include "fuses_pic16f628.c"
 #elif defined(_IS_PIC16F1827_CARD)
-#include "fuses_pic16f1827.inc"
+#include "fuses_pic16f1827.c"
 #elif defined(_IS_PIC16F887_CARD)
-#include "fuses_pic16f887.inc"
+#include "fuses_pic16f887.c"
 #else
 #error Missing configuration
 #endif
