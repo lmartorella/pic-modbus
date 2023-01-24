13b3de9619206ded1887e5b0c0984b0fcff32f5f
Luciano Martorella <mad_lux_it@yahoo.it>
2023-01-24 22:18:02 +0100

    - Removed protocol and sinks file

--- src/include/net/bus_client.h
+++ src/include/net/bus_client.h
@@ -26,11 +26,6 @@ void bus_cl_init();
  */
 extern uint8_t bus_cl_stationAddress;
 
-// RS485 Modbus defines station address in the range 1 to 247. 0 is used for broadcast messages without acknowledge.
-// So use 254 as special "unassigned" address. When the AUTO_REGISTER function is called, the only device in auto mode in the bus
-// should reply and change his address.
-#define UNASSIGNED_STATION_ADDRESS (254)
-
 /**
  * Poll for bus client activities.
  * Returns true if the node is currently active and requires short polling.
@@ -87,6 +82,11 @@ extern const FunctionDefinition bus_cl_functions[];
  * Specific state for RTU client
  ***
  **/
+
+// RS485 Modbus defines station address in the range 1 to 247. 0 is used for broadcast messages without acknowledge.
+// So use 254 as special "unassigned" address. When the AUTO_REGISTER function is called, the only device in auto mode in the bus
+// should reply and change his address.
+#define UNASSIGNED_STATION_ADDRESS 254
 
 typedef enum {
     // The client bus is idle, waiting for a complete frame header
