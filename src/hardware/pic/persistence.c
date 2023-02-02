de496346e4c48ea4ae870ae268b83b461d0dd751
Luciano Martorella <mad_lux_it@yahoo.it>
2023-02-02 22:22:38 +0100

    - Using new XC8 capabilites for __eeprom

--- src/hardware/pic/persistence.c
+++ src/hardware/pic/persistence.c
@@ -6,18 +6,7 @@
  */
 PersistentData pers_data;
 
-#ifdef _IS_ETH_CARD
-// pic18 doesn't have data EEPROM. Use a whole EEPROM code page, and allocate it whole
-#define __ADDRESS __at(0x1F800)
-static EEPROM_MODIFIER char s_persistentDataFiller[0x400 - sizeof(PersistentData)] __at(0x1F800 + sizeof(PersistentData));
-#define ROM_ADDR ((const void*)&s_persistentData)
-#else
-// pic16 XC8 doesn'c still support addressing __eeprom data. Assume it allocated at 0
-#define __ADDRESS
-#define ROM_ADDR 0
-#endif
-
-static EEPROM_MODIFIER PersistentData s_persistentData __ADDRESS = { 
+EEPROM_MODIFIER PersistentData rom_data __ADDRESS = { 
     // Zero GUID by default, it means unassigned
     .deviceId = { 0, 0, 0, 0, 0 }, 
 
@@ -29,11 +18,3 @@ static EEPROM_MODIFIER PersistentData s_persistentData __ADDRESS = {
     
     .custom = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 };
-
-void pers_load() {
-    rom_read(ROM_ADDR, (void*)&pers_data, sizeof(PersistentData));
-}
-
-void pers_save() {
-    rom_write(ROM_ADDR, (void*)&pers_data, sizeof(PersistentData));
-}
