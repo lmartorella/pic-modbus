ec6818e1dbdd29aa69909f0d58180616796d0ac6
Luciano Martorella <mad_lux_it@yahoo.it>
2023-06-21 22:50:22 +0200

    - Working log to telnet port

--- esp8266-bridge/src/main.ino
+++ esp8266-bridge/src/main.ino
@@ -1,12 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <ModbusTCP.h>
 #include <ModbusRTU.h>
+#include <TelnetStream.h>
 #include "wifi.h"
 
-static ModbusRTU rtu;
+// static ModbusRTU rtu;
 static ModbusTCP tcp;
-static HardwareSerial _log = Serial; // TX only
-static HardwareSerial _rtuSerial = Serial;
+static TelnetStreamClass& _log = TelnetStream; // TX only
+//static HardwareSerial& _rtuSerial = Serial;
 
 static const int MODBUS_TCP_PORT = 502;
 static uint16_t rtuTransId = 0;
@@ -18,20 +19,26 @@ static Modbus::ResultCode cbTcpRaw(uint8_t* data, uint8_t len, void* custom) {
   auto src = (Modbus::frame_arg_t*) custom;
   auto slaveId = src->slaveId;
   auto funCode = static_cast<Modbus::FunctionCode>(data[0]);
-  
-  _log.printf("TCP req: %d, Fn: %02X, len: %d\n: ", slaveId, funCode, len);
-  
+
+  // if (_log.available()) {
+  //   _log.printf("TCP req: %d, Fn: %02X, len: %d\n: ", slaveId, funCode, len);
+  // }
+
   // Must save transaction ans slave it for response processing
-  rtuTransId = rtu.rawRequest(slaveId, data, len);
+  //rtuTransId = rtu.rawRequest(slaveId, data, len);
   if (!rtuTransId) {
     // rawRequest returns 0 is unable to send data for some reason
-    _log.printf("err\n");
+    // if (_log.available()) {
+    //   _log.printf("err\n");
+    // }
     tcp.setTransactionId(src->transactionId); 
     tcp.errorResponce(IPAddress(src->ipaddr), funCode, Modbus::EX_DEVICE_FAILED_TO_RESPOND);
   } else {
     tcpTransId = src->transactionId;
     tcpIpaddr = src->ipaddr;
-    _log.printf("rtuTransId: %d\n", rtuTransId);
+    // if (_log.available()) {
+    //   _log.printf("rtuTransId: %d\n", rtuTransId);
+    // }
   }
   
   // Stop other processing
@@ -58,34 +65,49 @@ Modbus::ResultCode cbRtuRaw(uint8_t* data, uint8_t len, void* custom) {
   auto slaveId = src->slaveId;
   auto funCode = static_cast<Modbus::FunctionCode>(data[0]);
 
-  _log.printf("RTU: Fn: %02X, len: %d, transId: %d, to_server: %d\n", funCode, len, src->transactionId, src->to_server);
+  // if (_log.available()) {
+  //   _log.printf("RTU: Fn: %02X, len: %d, transId: %d, to_server: %d\n", funCode, len, src->transactionId, src->to_server);
+  // }
   if (!src->to_server && rtuTransId == src->transactionId) { // Check if transaction id is match
     tcp.rawResponce(IPAddress(tcpIpaddr), data, len);
     rtuTransId = 0;
     tcpTransId = 0;
     tcpIpaddr = 0;
   } else {
-    _log.printf("RTU: ignored, not in progress\n");
+    // if (_log.available()) {
+    //   _log.printf("RTU: ignored, not in progress\n");
+    // }
   }
   return Modbus::EX_SUCCESS; // Stop other processing
 }
 
 void setup() {
-  _log.begin(115200, SERIAL_8N1);
+  //_log.begin(115200, SERIAL_8N1);
+  
+  // WiFi.mode(WIFI_STA);
+  // WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+  WiFi.setHostname(WIFI_HOSTNAME);
   WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
-    
+
   tcp.server(MODBUS_TCP_PORT);
   tcp.onRaw(cbTcpRaw);
   tcp.onConnect(onTcpConnected);
   tcp.onDisconnect(onTcpDisconnected);
   
-  rtu.begin(&_rtuSerial);
-  rtu.master();
-  rtu.onRaw(cbRtuRaw); 
+  // rtu.begin(&_rtuSerial);
+  // rtu.master();
+  // rtu.onRaw(cbRtuRaw); 
+
+  TelnetStream.begin();
 }
 
 void loop() {
-  // rtu.task();
+  // Clear RX buffer
+  while (_log.available() > 0) {
+    _log.read();
+  }
+
+  //rtu.task();
   tcp.task();
   yield();
 }
