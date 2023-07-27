5a7f8787edb5aeef8a6f6af3d8675eda00ff7446
Luciano Martorella <mad_lux_it@yahoo.it>
2023-07-27 21:06:40 +0200

    - WIP dev

--- tests/lt8920/hw.cpp
+++ tests/lt8920/hw.cpp
@@ -1,8 +1,28 @@
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
 #include "hw.h"
 
 /**
- * Uses raspbian
+ * Uses linux dev
  */
+
+void hw_init() {
+    int fd = open("/dev/spidev0.0", O_RDWR);
+
+    struct spi_ioc_transfer tr =
+    {
+        .tx_buf = (unsigned long)tx,
+        .rx_buf = (unsigned long)rx,
+        .len = 1,
+        .delay_usecs = 0,
+        .speed_hz = 500000,
+        .bits_per_word = 8,
+    };
+
+    int status = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+    if (status < 0)
+        printf("can't send data");
+}
 
 void gpio_reset() {
     
